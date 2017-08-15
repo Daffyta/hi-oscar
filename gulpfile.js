d005deab820a78e37c170cda5d3c888d2422c117
Daffyta <daffy.dine9@gmail.com>
2017-08-15 11:14:59 -0500

    Initialization

---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+// var concat = require('gulp-concat');
+
+var config = {
+  source: './src/',
+  dist:'./public/'
+};
+
+var paths = {
+  // assets: "/assets/",
+  html: "**/*.html",
+  sass: 'scss/*.scss',
+  mainSass: 'scss/main.scss',
+  js: 'js/*.js'
+};
+
+var sources = {
+  // assets: config.source + paths.assets,
+  html: config.source + paths.html,
+  sass: config.source + paths.sass,
+  js: config.source + paths.js,
+};
+
+gulp.task('html', ()=> {
+  gulp.src(sources.html)
+  .pipe(gulp.dest(config.dist));
+});
+
+gulp.task("sass", function() {
+  gulp.src(sources.sass)
+    .pipe(sass({
+      outputStyle: "compressed"
+    }).on("error", sass.logError))
+    .pipe(gulp.dest(config.dist + "css"));
+});
+
+gulp.task('js', function() {
+  gulp.src(sources.js)
+    .pipe(concat('bundle.js'))
+    .pipe(gulp.dest(config.dist + "js"));
+});
